e4e005233984206cb99b8fe1c7ea9c3c52933671
j.faassen <j.faassen@linkorb.com>
2013-03-19 21:22:41 +0100

    Add comments in example code

--- examples/example.php
+++ examples/example.php
@@ -25,15 +25,22 @@ class Example {
 		$TransactionRequest->ReturnURL = 'http://www.linkorb.com/';
 		$TransactionRequest->StartRecurrent = FALSE;
 
+		// Specify which service / action we are calling
 		$TransactionRequest->Services = new SOAP\Services();
-		$TransactionRequest->Services->Service = new SOAP\Service('ideal', 'Pay', 1);
+		$TransactionRequest->Services->Service 
+			= new SOAP\Service('ideal', 'Pay', 1);
 
+		// Add parameters for this service
+		$TransactionRequest->Services->Service->RequestParameter 
+			= new SOAP\RequestParameter('issuer', '0031');
+
+		// Optionally pass the client ip-address for logging
 		$TransactionRequest->ClientIP = new SOAP\IPAddress('123.123.123.123');
-
-		$TransactionRequest->Services->Service->RequestParameter = new SOAP\RequestParameter('issuer', '0031');
 
 		// Send the request to Buckaroo, and retrieve the response
 		$response = $req->sendTransactionRequest($TransactionRequest);
+
+		// Display the response:
 		var_dump($response);
 	}
 }
